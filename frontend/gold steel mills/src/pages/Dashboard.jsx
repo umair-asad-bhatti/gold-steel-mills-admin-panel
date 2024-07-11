2bab9ae3cfe7da24ff5070052d6b5f69d574f654
umair-asad-bhatti <umaircyber6@gmail.com>
2024-07-11 14:20:28 +0500

    Added the charts on dashboard

--- frontend/gold steel mills/src/pages/Dashboard.jsx
+++ frontend/gold steel mills/src/pages/Dashboard.jsx
@@ -1,9 +1,22 @@
 import {PageHeader} from "../components/PageHeader.jsx";
-
+import {Bar} from 'react-chartjs-2'
+import {Chart } from 'chart.js/auto'
 export const Dashboard=()=>{
     return (
         <div>
             <PageHeader title={'Dashboard'}/>
+            <div className={'w-96 h-96'}>
+
+                <Bar
+                data={{
+                    labels:['A','B','C'],
+                    datasets:[{
+                        label:'revenue',
+                        data:[100,200,300]}
+                    ]}
+                }
+                />
+            </div>
         </div>
     )
 }
