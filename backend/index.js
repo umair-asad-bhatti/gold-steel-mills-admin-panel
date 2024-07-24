022e30fc6a00a09bbabb1b3eb8fc17aae8e3a5a8
umair-asad-bhatti <umaircyber6@gmail.com>
2024-07-24 15:24:16 +0500

    added the user in users table

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
 
-const {ValidateRequest} = require("./middleware/ValidateRquest");
-const {Database}=require('./database/Database');
-const {config}=require("./database/config")
+const { ValidateRequest } = require("./middleware/ValidateRquest");
+const { Database } = require('./database/Database');
+const { config } = require("./database/config")
 new Database(config);
 
-const express=require('express')
-const bodyParser=require('body-parser')
-const cors=require('cors')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-const app=express();
-const {router}=require("./router/router")
-const {user} = require("./models/user");
+const app = express();
+const { router } = require("./router/router")
+const { user } = require("./models/user");
 
 //middlewares
 app.use(cors())
@@ -19,13 +19,19 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 // app.use(ValidateRequest);
 
-app.use("/api/v1",router);
+app.use("/api/v1", router);
 
-const port=process.env.SERVER_PORT||8080;
-app.listen(port,async function(){
+const port = process.env.SERVER_PORT || 8080;
+app.listen(port, async function () {
     await Database.sequelize.sync()
     await user.sync();
-    console.log('server is listening and running on port',port);
+    await user.findOrCreate({
+        where: { username: 'admin' },
+        defaults: {
+            username: 'admin', password: 'admin'
+        },
+    });
+    console.log('server is listening and running on port', port);
 })
 
 
